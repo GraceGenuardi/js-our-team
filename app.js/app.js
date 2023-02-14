b68c9ae367873db4aa7443fbd35e3ed8711a6c35
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-14 14:05:31 +0100

    EDIT struttura HTML

---
+++ app.js/app.js
@@ -0,0 +1,10 @@
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto (in questo fase usiamo il ciclo for…in )
+
+
+
+//MILESTONE 2:
+//Stampare le stesse informazioni su DOM sotto forma di stringhe
